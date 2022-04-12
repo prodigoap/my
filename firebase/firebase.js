694e03f1b11e2178d00ca1a386aeb2c35d975de6
prodigoap <sandrounicam@hotmail.it>
2022-04-12 18:36:48 +0200

    some fixes

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -2,6 +2,8 @@ import firebaseConfig from "./firebaseConfig";
 import { initializeApp, getApp } from "firebase/app";
 import { getFirestore}  from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import firebase from "./firebase"
+import "firebase/firestore";
 
 const createFirebaseApp = ( firebaseConfig) => {
   try {
@@ -14,6 +16,17 @@ const createFirebaseApp = ( firebaseConfig) => {
 const firebaseApp = createFirebaseApp(firebaseConfig)
 const db = getFirestore(firebaseApp);
 const auth = getAuth(firebaseApp );
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    const uid = user.uid;
+    // ...
+  } else {
+    // User is signed out
+    // ...
+  }
+});
 export default db;
 export const signIn = (email, password) =>
   firebase.auth().signInWithEmailAndPassword(email, password);
@@ -22,3 +35,4 @@ export const resetPassword = (email) =>
   firebase.auth().sendPasswordResetEmail(email);
 
 export const getToken = () => firebase.auth().currentUser.getIdToken();
+
--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -1,13 +1,15 @@
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB-VKNKZkc9vNlcgyWBQqxX8FMks-XA6jI",
-    authDomain: "keycense.firebaseapp.com",
-    databaseURL: "https://keycense-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "keycense",
-    storageBucket: "keycense.appspot.com",
-    messagingSenderId: "569416049848",
-    appId: "1:569416049848:web:8606d4eedff29bbcaf004d"
-  };
+  apiKey: "AIzaSyB-VKNKZkc9vNlcgyWBQqxX8FMks-XA6jI",
+  authDomain: "keycense.firebaseapp.com",
+  databaseURL: "https://keycense-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "keycense",
+  storageBucket: "keycense.appspot.com",
+  messagingSenderId: "569416049848",
+  appId: "1:569416049848:web:8606d4eedff29bbcaf004d"
+};
+
+
 
   export default firebaseConfig;
 /*
